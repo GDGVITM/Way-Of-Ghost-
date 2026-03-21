32c9e6db9700d659882eab018eca8bcdaeff5bc9
TanishqChavan10 <tan20051007@gmail.com>
2026-03-21 16:38:39 +0530

    fix: listen on 0.0.0.0 for deployment

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,6 @@ import { createSocketServer } from "./socket/index.js";
 const server = http.createServer(app);
 createSocketServer(server);
 
-server.listen(env.PORT, () => {
-  console.log(`Server running on http://localhost:${env.PORT}`);
+server.listen(env.PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${env.PORT}`);
 });
